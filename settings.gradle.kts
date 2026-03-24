64f3b920048980c1134e385f457cdcab648966b5
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2026-03-24 13:57:39 +0100

    Remove plugin management settings for github
    The plugins are now available at Gradle Plugin Portal

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,3 @@
-pluginManagement {
-    val githubUsername: String? by settings
-    val githubToken: String? by settings
-
-    repositories {
-        mavenLocal()
-        maven {
-            url = uri("https://maven.pkg.github.com/dsdolzhenko/gradle-secrets-plugin")
-            credentials {
-                username = githubUsername ?: System.getenv("GITHUB_USERNAME")
-                password = githubToken ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
